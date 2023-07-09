35e6b26d91f3aa2815b9970edacef24cb988a5cb
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-07-09 14:18:06 +0200

    [added] login page

--- frontend/src/pages/pages.module.ts
+++ frontend/src/pages/pages.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
+import { CommonModule, NgOptimizedImage } from '@angular/common'
 import { LoginPageComponent } from './login-page/login-page.component'
 
 @NgModule({
@@ -10,7 +10,8 @@ import { LoginPageComponent } from './login-page/login-page.component'
     LoginPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgOptimizedImage
   ]
 })
 export class PagesModule {
