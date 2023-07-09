0045ce9eaa4072ba2c9ad0f0e8e34d975da6011e
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-07-09 11:49:46 +0200

    [added] karma config and firefox config for testing

---
+++ frontend/karma.conf.js
@@ -0,0 +1,34 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+      },
+      clearContext: false
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageReporter: {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      dir: require('path').join(__dirname, './coverage/fillist'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    restartOnFileChange: true
+  })
+}
