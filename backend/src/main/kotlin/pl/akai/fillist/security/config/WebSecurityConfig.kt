6d5a89b78883532d0cca069c68c902bcde296a4d
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-08-07 10:23:52 +0200

    [improved] migrate to webflux and remove OauthProperties

---
+++ backend/src/main/kotlin/pl/akai/fillist/security/config/WebSecurityConfig.kt
@@ -0,0 +1,28 @@
+package pl.akai.fillist.security.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
+
+
+@Configuration
+@EnableWebFluxSecurity
+class WebSecurityConfig {
+
+    @Bean
+    fun apiHttpSecurity(http: ServerHttpSecurity): SecurityWebFilterChain {
+        http
+            .authorizeExchange { exchanges: AuthorizeExchangeSpec ->
+                exchanges.pathMatchers("/oauth2/**").permitAll()
+                exchanges.anyExchange().authenticated()
+            }
+            .formLogin{ it.disable() }
+            .httpBasic { it.authenticationEntryPoint(HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)) }
+        return http.build()
+    }
+}
