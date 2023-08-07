0e658f34c46817789c2c5f6e121de12934c0e835
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-08-07 11:54:03 +0200

    [removed] unused service oauth-code

--- frontend/src/security/oauth-code/model/oauth-code-params.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface OauthCodeParams {
-  client_id: string
-  response_type: string
-  redirect_uri: string
-  scope: string
-  state: string
-}
--- frontend/src/security/oauth-code/model/oauth-properties.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface OauthProperties {
-  clientId: string
-  scopes: string[]
-}
