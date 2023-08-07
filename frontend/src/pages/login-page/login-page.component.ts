d238b3033bb41f45d4021d48059d7446216619d2
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-08-07 11:53:19 +0200

    [removed] WebConfig.kt for mvc

--- frontend/src/pages/login-page/login-page.component.ts
+++ frontend/src/pages/login-page/login-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, type OnInit } from '@angular/core'
-import { OAuthCodeService } from '../../security/oauth-code/service/oauth-code.service'
-import { Observable } from 'rxjs'
 
 @Component({
   selector: 'fillist-login-page',
@@ -8,11 +6,6 @@ import { Observable } from 'rxjs'
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-  loginUrl$: Observable<string> | undefined
-  constructor (protected oAuthCodeService: OAuthCodeService) {
-  }
-
   ngOnInit (): void {
-    this.loginUrl$ = this.oAuthCodeService.getAuthorizationCodeUrl()
   }
 }
