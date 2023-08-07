b4f4c42e172cd5d62f9b1b4f51e3e97d3283ee56
GitHub Action <noreply@akai.org>
2023-08-07 10:06:16 +0000

    [CI] Automatic Ktlint Fix

--- backend/src/main/kotlin/pl/akai/fillist/security/login/models/OAuthParams.kt
+++ backend/src/main/kotlin/pl/akai/fillist/security/login/models/OAuthParams.kt
@@ -7,10 +7,13 @@ import org.springframework.stereotype.Component
 class OAuthParams {
     @Value("\${fillist.oauth2.client.registration.spotify.client-id}")
     lateinit var clientId: String
-    @Value ("\${fillist.oauth2.client.registration.spotify.authorization-uri}")
+
+    @Value("\${fillist.oauth2.client.registration.spotify.authorization-uri}")
     lateinit var authorizationUri: String
-    @Value ("\${fillist.oauth2.client.registration.spotify.redirect-uri}")
+
+    @Value("\${fillist.oauth2.client.registration.spotify.redirect-uri}")
     lateinit var redirectUri: String
-    @Value ("\${fillist.oauth2.client.registration.spotify.scope}")
+
+    @Value("\${fillist.oauth2.client.registration.spotify.scope}")
     lateinit var scope: String
 }
--- backend/src/main/kotlin/pl/akai/fillist/security/login/service/Oauth2LoginService.kt
+++ backend/src/main/kotlin/pl/akai/fillist/security/login/service/Oauth2LoginService.kt
@@ -9,7 +9,7 @@ import pl.akai.fillist.security.login.models.OAuthParams
 class Oauth2LoginService @Autowired constructor(val redirectUrlParams: OAuthParams) {
 
     fun getAuthorizationCodeUrl(state: String): AuthorizationCodeUrlResponseBody {
-        val url = "${redirectUrlParams.authorizationUri}?client_id=${redirectUrlParams.clientId}&response_type=code&redirect_uri=${redirectUrlParams.redirectUri}&scope=${redirectUrlParams.scope}&state=${state}"
+        val url = "${redirectUrlParams.authorizationUri}?client_id=${redirectUrlParams.clientId}&response_type=code&redirect_uri=${redirectUrlParams.redirectUri}&scope=${redirectUrlParams.scope}&state=$state"
         return AuthorizationCodeUrlResponseBody(url)
     }
 }
