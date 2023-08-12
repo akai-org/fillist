871992c3d176282f966fd1c310c477011657530e
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-08-12 21:35:34 +0200

    [improved] building authorizationCodeUrl

--- backend/src/main/kotlin/pl/akai/fillist/security/sso/models/OAuthParams.kt
+++ backend/src/main/kotlin/pl/akai/fillist/security/sso/models/OAuthParams.kt
@@ -2,6 +2,9 @@ package pl.akai.fillist.security.sso.models
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+
 
 @Component
 class OAuthParams {
@@ -16,4 +19,12 @@ class OAuthParams {
 
     @Value("\${fillist.oauth2.client.registration.spotify.scope}")
     lateinit var scope: String
+
+    fun toMultiValueMap(): MultiValueMap<String, String> {
+        val map: MultiValueMap<String, String> = LinkedMultiValueMap()
+        map.add("client_id", clientId)
+        map.add("redirect_uri", redirectUri)
+        map.add("scope", scope)
+        return map
+    }
 }
