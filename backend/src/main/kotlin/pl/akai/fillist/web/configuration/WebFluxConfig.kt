fd19307560c8250ab7e5e9e6abb364f03d3999c0
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-08-12 19:47:02 +0200

    [added] hypermedia type HAL

--- backend/src/main/kotlin/pl/akai/fillist/web/configuration/WebFluxConfig.kt
+++ backend/src/main/kotlin/pl/akai/fillist/web/configuration/WebFluxConfig.kt
@@ -1,9 +1,11 @@
 package pl.akai.fillist.web.configuration
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.hateoas.config.EnableHypermediaSupport
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
 
 @Configuration
 @EnableWebFlux
+@EnableHypermediaSupport(type = [EnableHypermediaSupport.HypermediaType.HAL])
 class WebFluxConfig : WebFluxConfigurer
