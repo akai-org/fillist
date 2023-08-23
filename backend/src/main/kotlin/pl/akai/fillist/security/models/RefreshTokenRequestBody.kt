f2a668650fc00085064d9b468b3460722a3aa424
GitHub Action <noreply@akai.org>
2023-08-23 08:55:55 +0000

    [CI] Automatic Ktlint Fix

--- backend/src/main/kotlin/pl/akai/fillist/security/models/RefreshTokenRequestBody.kt
+++ backend/src/main/kotlin/pl/akai/fillist/security/models/RefreshTokenRequestBody.kt
@@ -6,8 +6,8 @@ import org.springframework.util.LinkedMultiValueMap
 @Serializable
 data class RefreshTokenRequestBody(
     private val grantType: String,
-    private val refreshToken: String
-){
+    private val refreshToken: String,
+) {
     fun toLinkedMultiValueMap(): LinkedMultiValueMap<String, String> {
         return LinkedMultiValueMap<String, String>().apply {
             add("grant_type", grantType)
