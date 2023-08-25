f4ebb5682150408eb1173ef95cf59b8d8c8c1cb1
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-08-25 20:17:04 +0200

    [improved] backend testing

--- backend/src/test/kotlin/pl/akai/fillist/security/Oauth2AuthorizationCodeServiceTests.kt
+++ backend/src/test/kotlin/pl/akai/fillist/security/Oauth2AuthorizationCodeServiceTests.kt
@@ -1,8 +1,10 @@
 package pl.akai.fillist.security
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import pl.akai.fillist.security.models.OAuthParams
 import pl.akai.fillist.security.service.Oauth2AuthorizationCodeService
 
 @SpringBootTest
@@ -10,10 +12,18 @@ class Oauth2AuthorizationCodeServiceTests {
     @Autowired
     private lateinit var oauth2AuthorizationCodeService: Oauth2AuthorizationCodeService
 
+    @Autowired
+    private lateinit var oAuthParams: OAuthParams
+
     @Test
     fun getAuthorizationCodeUrl() {
-        oauth2AuthorizationCodeService.getAuthorizationCodeUrl("state").subscribe {
-            assert(it.url == "https://accounts.spotify.com/authorize?client_id=clientId&response_type=code&redirect_uri=http://localhost:8080/oauth2/token&state=state")
+        oauth2AuthorizationCodeService.getAuthorizationCodeUrl("state").block()?.let {
+            val spotifyIdpUrl = it.url.split("?")[0]
+            val queryParams = it.url.split("?")[1].split("&").map { it.split("=")[0] to it.split("=")[1] }.toMap()
+            assertEquals("${oAuthParams.spotifyIdpUri}/authorize", spotifyIdpUrl)
+            assertEquals(queryParams["client_id"], oAuthParams.clientId)
+            assertEquals(queryParams["redirect_uri"], oAuthParams.redirectUri)
+            assertEquals(queryParams["scope"], oAuthParams.scope)
         }
     }
 }
