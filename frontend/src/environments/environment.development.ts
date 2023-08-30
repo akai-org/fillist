b35742322f48fbb5f915a4caa208c2626b24e43b
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-08-30 23:02:25 +0200

    [added] fade in mixin

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  backendUrl: 'http://192.168.176.52:8080',
-  frontendUrl: 'http://192.168.176.52:4200',
+  backendUrl: 'http://localhost:8080',
+  frontendUrl: 'http://localhost:4200',
   redirectPath: '/callback'
 }
