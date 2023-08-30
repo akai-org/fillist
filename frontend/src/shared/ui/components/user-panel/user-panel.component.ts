33f83a7595e09cd0f846ef9028e5a940b0f11012
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-08-30 11:31:19 +0200

    [added] display user image

--- frontend/src/shared/ui/components/user-panel/user-panel.component.ts
+++ frontend/src/shared/ui/components/user-panel/user-panel.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { UserProfileService } from '../../services/user-profile.service'
+import { map, Observable } from 'rxjs'
 
 @Component({
   selector: 'fillist-user-panel',
   templateUrl: './user-panel.component.html',
   styleUrls: ['./user-panel.component.scss']
 })
-export class UserPanelComponent {
+export class UserPanelComponent implements OnInit {
+  smallImage: Observable<string> = this.userPanelService.userProfile.pipe(map(it => it.smallImageUrl))
 
+  constructor (private userPanelService: UserProfileService) {
+  }
+
+  ngOnInit (): void {
+    this.userPanelService.userProfile.subscribe(it => { console.log(it) })
+  }
 }
