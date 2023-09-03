a652054df8892645f3e24c0c31169deccad9ac72
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-09-03 12:34:39 +0200

    [added] frontend tests

--- frontend/src/pages/playlists-view/playlist-tile/playlist-tile.component.spec.ts
+++ frontend/src/pages/playlists-view/playlist-tile/playlist-tile.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { PlaylistTileComponent } from './playlist-tile.component'
+import { NgOptimizedImage } from '@angular/common'
 
 describe('PlaylistTileComponent', () => {
   let component: PlaylistTileComponent
@@ -8,14 +9,26 @@ describe('PlaylistTileComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PlaylistTileComponent]
+      declarations: [PlaylistTileComponent],
+      imports: [NgOptimizedImage]
     })
     fixture = TestBed.createComponent(PlaylistTileComponent)
     component = fixture.componentInstance
+    component.name = 'name'
+    component.image = 'image'
+    component.owner = 'owner'
+    component.id = 'id'
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should have the correct values', () => {
+    const compiled = fixture.nativeElement
+    expect(compiled.querySelector('img').src).toContain('image')
+    expect(compiled.querySelector('h6').textContent).toContain('name')
+    expect(compiled.querySelector('small').textContent).toContain('owner')
   })
 })
--- frontend/src/pages/playlists-view/playlists-view.component.spec.ts
+++ frontend/src/pages/playlists-view/playlists-view.component.spec.ts
@@ -1,25 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { PlaylistsViewComponent } from './playlists-view.component'
-import { HttpClient, HttpHandler } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 import { UiModule } from '../../shared/ui/ui.module'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { environment } from '../../environments/environment'
+import { PlaylistsResponseBody } from './playlists-response-body.interface'
 
 describe('PlaylistsViewComponent', () => {
   let component: PlaylistsViewComponent
   let fixture: ComponentFixture<PlaylistsViewComponent>
-
+  let httpTestingController: HttpTestingController
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [PlaylistsViewComponent],
-      providers: [HttpClient, HttpHandler],
-      imports: [UiModule]
+      providers: [HttpClient],
+      imports: [UiModule, HttpClientTestingModule]
     })
     fixture = TestBed.createComponent(PlaylistsViewComponent)
+    httpTestingController = TestBed.inject(HttpTestingController)
     component = fixture.componentInstance
     fixture.detectChanges()
   })
 
+  afterEach(() => {
+    httpTestingController.verify()
+  })
+
   it('should create', () => {
+    const mockResponse: PlaylistsResponseBody = {
+      limit: 1,
+      offset: 0,
+      total: 1,
+      playlists: [
+        {
+          name: 'name',
+          id: 'id',
+          image: 'image',
+          ownerDisplayName: 'ownerDisplayName'
+        }
+      ]
+    }
+    httpTestingController.expectOne(`${environment.backendUrl}/playlists`).flush(mockResponse)
     expect(component).toBeTruthy()
+    expect(component.playlistsResponse).toEqual(mockResponse)
   })
 })
