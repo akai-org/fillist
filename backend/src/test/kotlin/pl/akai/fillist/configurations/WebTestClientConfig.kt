12cde16060bcd1431cd576e2499ecd9a2082f3b7
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-09-06 12:30:11 +0200

    [added] new tests which is possible by new token

--- backend/src/test/kotlin/pl/akai/fillist/configurations/WebTestClientConfig.kt
+++ backend/src/test/kotlin/pl/akai/fillist/configurations/WebTestClientConfig.kt
@@ -1,21 +1,14 @@
 package pl.akai.fillist.configurations
 
 import AccessTokenResponseBody
-import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.test.web.reactive.server.WebTestClientBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
 import org.springframework.http.HttpHeaders
 import org.springframework.test.web.reactive.server.WebTestClient
 import pl.akai.fillist.security.service.TokenService
-import pl.akai.fillist.web.spotifywrapper.models.Image
-import pl.akai.fillist.web.spotifywrapper.user.SpotifyProfileResponseBody
-import pl.akai.fillist.web.spotifywrapper.user.SpotifyUserService
-import reactor.core.publisher.Mono
 
 @TestConfiguration
 class WebTestClientConfig {
@@ -25,12 +18,6 @@ class WebTestClientConfig {
 
     @Autowired
     private lateinit var tokenService: TokenService
-
-    @Value("\${fillist.test.spotify.user-id}")
-    private lateinit var id: String
-
-    @MockBean
-    lateinit var spotifyUserService: SpotifyUserService
 
     @Bean
     @Primary
@@ -44,22 +31,7 @@ class WebTestClientConfig {
     }
 
     private fun getFillistToken(spotifyToken: AccessTokenResponseBody): String {
-        this.configMock()
         return tokenService.generateFillistAccessToken(spotifyToken).block()!!
     }
 
-    fun configMock() {
-        Mockito.`when`(spotifyUserService.getProfile(spotifyToken.accessToken)).thenReturn(
-            Mono.just(
-                SpotifyProfileResponseBody(
-                    id,
-                    "email",
-                    "displayName",
-                    listOf(
-                        Image("url", 100, 100),
-                    ),
-                ),
-            ),
-        )
-    }
 }
---
+++ backend/src/test/kotlin/pl/akai/fillist/web/spotifywrapper/SpotifyUsersTests.kt
@@ -0,0 +1,32 @@
+package pl.akai.fillist.web.spotifywrapper
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ContextConfiguration
+import pl.akai.fillist.configurations.SpotifyClientConfig
+import pl.akai.fillist.web.spotifywrapper.user.SpotifyUserService
+
+@SpringBootTest()
+@Import(SpotifyClientConfig::class)
+@ContextConfiguration(classes = [SpotifyClientConfig::class])
+class SpotifyUsersTests {
+    @Autowired
+    private lateinit var spotifyUserService: SpotifyUserService
+
+    @Value("\${fillist.test.spotify.user-id}")
+    private lateinit var id: String
+
+    @Test
+    fun getProfile() {
+        val profile = spotifyUserService.getProfile().block()!!
+        assertEquals(profile.id, id)
+        assertNotNull(profile.email)
+        assertNotNull(profile.displayName)
+        assertNotNull(profile)
+    }
+}
