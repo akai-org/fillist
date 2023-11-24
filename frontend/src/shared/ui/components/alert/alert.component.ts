6b57e64f8befde1fd28b14e596fa5206774ce8ce
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2023-11-24 16:21:19 +0100

    Removing useless interface

--- frontend/src/shared/ui/components/alert/alert.component.ts
+++ frontend/src/shared/ui/components/alert/alert.component.ts
@@ -1,13 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core'
 import { AlertColor } from './alert-color'
-import { DialogComponent } from '../../models/dialog-component.interface'
 
 @Component({
   selector: 'fillist-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.scss']
 })
-export class AlertComponent implements DialogComponent {
+export class AlertComponent {
   @Output() public onClose = new EventEmitter<() => void>()
   @Input() public message: string = 'An error occurred!'
   @Input() public type: AlertColor = AlertColor.ERROR
--- frontend/src/shared/ui/models/dialog-component.interface.ts
+++
@@ -1,5 +0,0 @@
-import { EventEmitter } from '@angular/core'
-
-export interface DialogComponent {
-  onClose: EventEmitter<() => void>
-}
