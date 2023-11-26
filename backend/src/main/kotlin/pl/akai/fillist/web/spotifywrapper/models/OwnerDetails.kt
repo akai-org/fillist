998e3d7d4d3dede2800b540dfbd183ca643f5c20
Szymon Skowroński <szymon.skowronski.uczen17@zsl.poznan.pl>
2023-11-26 01:36:42 +0100

    minor changes

---
+++ backend/src/main/kotlin/pl/akai/fillist/web/spotifywrapper/models/OwnerDetails.kt
@@ -0,0 +1,10 @@
+package pl.akai.fillist.web.spotifywrapper.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class OwnerDetails(
+    val id: String,
+    val name: String,
+    val picture: String?
+)
