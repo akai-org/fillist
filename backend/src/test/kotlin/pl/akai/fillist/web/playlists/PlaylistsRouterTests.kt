fece78206da1ec35776a5eba2c9bda2d47f93038
Szymon Skowroński <szymon.skowronski.uczen17@zsl.poznan.pl>
2024-02-27 19:19:04 +0100

    fix: ktlint

--- backend/src/test/kotlin/pl/akai/fillist/web/playlists/PlaylistsRouterTests.kt
+++ backend/src/test/kotlin/pl/akai/fillist/web/playlists/PlaylistsRouterTests.kt
@@ -203,7 +203,7 @@ class PlaylistsRouterTests {
         val playlistId = "1B9WyPlzPbkyGMWcGlrgP7"
         val image = "/9j/2wCEABoZGSccJz4lJT5CLy8vQkc9Ozs9R0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0cBHCcnMyYzPSYmPUc9Mj1HR0dEREdHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR//dAAQAAf/uAA5BZG9iZQBkwAAAAAH/wAARCAABAAEDACIAAREBAhEB/8QASwABAQAAAAAAAAAAAAAAAAAAAAYBAQAAAAAAAAAAAAAAAAAAAAAQAQAAAAAAAAAAAAAAAAAAAAARAQAAAAAAAAAAAAAAAAAAAAD/2gAMAwAAARECEQA/AJgAH//Z"
         `when`(playlistsService.changePlaylistCover(eq(playlistId), eq(image))).thenReturn(
-            Mono.empty()
+            Mono.empty(),
         )
         webTestClient.put().uri("/playlists/$playlistId/cover").body(Mono.just(image))
             .exchange()
